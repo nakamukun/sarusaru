eef8262c883c783c4da02d763a515da6da826b1b
nakamukun <gotmonkey1227@gmail.com>
2023-08-16 14:18:50 +0900

    ブログ投稿詳細画面実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,5 +11,12 @@ class PostController extends Controller
      public function index(Post $post)
   {
      return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+
   }
+  
+  public function show(Post $post)
+{
+     return view('posts/show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+}
 }
